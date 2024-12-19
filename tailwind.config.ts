95770a39196a1d2cdbce66ff655426eb352344ed
GalangHarindito <galang.harindito@gmail.com>
2024-12-19 19:59:09 +0700

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,10 +11,20 @@ export default {
       colors: {
         "checkbox-blue": '#1755FF',
         "checkbox-gray-border": '#949494',
+        "header-blue": '#281E9B',
+        background: "var(--background)",
+        'background-main': '#F8F8F8',
         foreground: "var(--foreground)",
       },
       borderWidth: {
         DEFAULT: '1px',
+      },
+      borderRadius: {
+        'none': '0',
+        'round-30px': '1.875rem',
+      },
+      padding: {
+        '12.5': '3.125rem',
       }
     },
   },
