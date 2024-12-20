a36d492d1d0025de2518fa54224758b1d4650a14
GalangHarindito <galang.harindito@gmail.com>
2024-12-20 15:35:44 +0700

    [component]fixing Typography component

--- src/app/shared/components/typography/__test__/typography.test.tsx
+++ src/app/shared/components/typography/__test__/typography.test.tsx
@@ -1,70 +1,89 @@
-import { render } from "@testing-library/react";
-import Typography from "../typography";
-import "@testing-library/jest-dom";
+import { render } from '@testing-library/react';
+import Typography from '../typography';
+import '@testing-library/jest-dom';
 
+describe('Typography Component', () => {
+  test('Component header h1', () => {
+    const { getByText, container } = render(
+      <Typography type="H1" title="Heading 1" />
+    );
+    const heading = getByText('Heading 1');
+    expect(heading.tagName).toBe('H1');
+    expect(heading).toHaveClass(
+      'font-bold xl:text-4xl lg:text-4xl md:text-2xl'
+    );
+    expect(container).toMatchSnapshot();
+  });
 
-describe('Typography Component',() => {
+  test('Component header h2', () => {
+    const { getByText, container } = render(
+      <Typography type="H2" title="Heading 2" />
+    );
+    const heading = getByText('Heading 2');
+    expect(heading.tagName).toBe('H2');
+    expect(heading).toHaveClass('font-bold  xl:text-3xl text-2xl');
+    expect(container).toMatchSnapshot();
+  });
 
-    test('Component header h1', () => {
-        const { getByText, container } = render(<Typography type="H1">Heading 1</Typography>)
-        const heading = getByText('Heading 1')
-        expect(heading.tagName).toBe('H1')
-        expect(heading).toHaveClass('font-bold xl:text-4xl lg:text-4xl md:text-2xl')
-        expect(container).toMatchSnapshot();
-    })
+  test('Component header h3', () => {
+    const { getByText, container } = render(
+      <Typography type="H3" title="Heading 3" />
+    );
+    const heading = getByText('Heading 3');
+    expect(heading.tagName).toBe('H3');
+    expect(heading).toHaveClass('font-bold xl:text-2xl lg:text-xl md:text-lg');
+    expect(container).toMatchSnapshot();
+  });
 
-    test('Component header h2', () => {
-        const { getByText, container } = render(<Typography type="H2">Heading 2</Typography>)
-        const heading = getByText('Heading 2')
-        expect(heading.tagName).toBe('H2')
-        expect(heading).toHaveClass('font-bold xl:text-3xl lg:text-2xl md:text-xl')
-        expect(container).toMatchSnapshot();
-    })
+  test('Component header h4', () => {
+    const { getByText, container } = render(
+      <Typography type="H4" title="Heading 4" />
+    );
+    const heading = getByText('Heading 4');
+    expect(heading.tagName).toBe('H4');
+    expect(heading).toHaveClass('font-bold xl:text-xl lg:text-lg');
+    expect(container).toMatchSnapshot();
+  });
 
-    test('Component header h3', () => {
-        const { getByText, container } = render(<Typography type="H3">Heading 3</Typography>)
-        const heading = getByText('Heading 3')
-        expect(heading.tagName).toBe('H3')
-        expect(heading).toHaveClass('font-bold xl:text-2xl lg:text-xl md:text-lg')
-        expect(container).toMatchSnapshot();
-    })
+  test('Component header h5', () => {
+    const { getByText, container } = render(
+      <Typography type="H5" title="Heading 5" />
+    );
+    const heading = getByText('Heading 5');
+    expect(heading.tagName).toBe('H5');
+    expect(heading).toHaveClass('font-bold xl:text-lg lg:text-base');
+    expect(container).toMatchSnapshot();
+  });
 
-    test('Component header h4', () => {
-        const { getByText, container } = render(<Typography type="H4">Heading 4</Typography>)
-        const heading = getByText('Heading 4')
-        expect(heading.tagName).toBe('H4')
-        expect(heading).toHaveClass('font-bold xl:text-xl lg:text-lg')
-        expect(container).toMatchSnapshot();
-    })
+  test('Component header h6', () => {
+    const { getByText, container } = render(
+      <Typography type="H6" title="Heading 6" />
+    );
+    const heading = getByText('Heading 6');
+    expect(heading.tagName).toBe('H6');
+    expect(heading).toHaveClass('font-bold xl:text-base');
+    expect(container).toMatchSnapshot();
+  });
 
-    test('Component header h5', () => {
-        const { getByText, container } = render(<Typography type="H5">Heading 5</Typography>)
-        const heading = getByText('Heading 5')
-        expect(heading.tagName).toBe('H5')
-        expect(heading).toHaveClass('font-bold xl:text-lg lg:text-base')
-        expect(container).toMatchSnapshot();
-    })
+  test('Component header p', () => {
+    const { getByText, container } = render(
+      <Typography type="p" title="This is a paragraph" />
+    );
+    const heading = getByText('This is a paragraph');
+    expect(heading.tagName).toBe('P');
+    expect(heading).not.toHaveClass('font-bold');
+    expect(container).toMatchSnapshot();
+  });
 
-    test('Component header h6', () => {
-        const { getByText, container } = render(<Typography type="H6">Heading 6</Typography>)
-        const heading = getByText('Heading 6')
-        expect(heading.tagName).toBe('H6')
-        expect(heading).toHaveClass('font-bold xl:text-base')
-        expect(container).toMatchSnapshot();
-    })
-
-    test('Component header p', () => {
-        const { getByText, container } = render(<Typography type="p">This is a paragraph</Typography>)
-        const heading = getByText('This is a paragraph')
-        expect(heading.tagName).toBe('P')
-        expect(heading).not.toHaveClass('font-bold')
-        expect(container).toMatchSnapshot();
-    })
-
-    it('applies additional class names', () => {
-        const { getByText } = render(<Typography type="H1" className={['custom-class']}>Heading with custom class</Typography>);
-        const heading = getByText('Heading with custom class');
-        expect(heading).toHaveClass('custom-class');
-      });
-    
-}) 
+  it('applies additional class names', () => {
+    const { getByText } = render(
+      <Typography
+        type="H1"
+        className={['custom-class']}
+        title="Heading with custom class"
+      />
+    );
+    const heading = getByText('Heading with custom class');
+    expect(heading).toHaveClass('custom-class');
+  });
+});
--- src/app/shared/components/typography/typography.tsx
+++ src/app/shared/components/typography/typography.tsx
@@ -1,38 +1,72 @@
 interface HeadingProps {
   type: string;
   children?: React.ReactNode;
+  title: string;
   className?: string[];
 }
 
 export default function Typography(props: HeadingProps) {
-  const { type, children, className } = props;
+  const { type, title, className } = props;
   let content;
 
   switch (type) {
-    case "H1":
-      content = <h1 className={`${className?.join(" ") || ''} font-bold  xl:text-4xl lg:text-4xl md:text-2xl`}>{children}</h1>;
+    case 'H1':
+      content = (
+        <h1
+          className={`${className?.join(' ') || ''} font-bold  xl:text-4xl lg:text-4xl md:text-2xl`}
+        >
+          {title}
+        </h1>
+      );
       break;
-    case "H2":
-      content = <h2 className={`${className?.join(" ") || ''} font-bold  xl:text-3xl lg:text-2xl md:text-xl`}>{children}</h2>;
+    case 'H2':
+      content = (
+        <h2
+          className={`${className?.join(' ') || ''} font-bold  xl:text-3xl text-2xl`}
+        >
+          {title}
+        </h2>
+      );
       break;
-    case "H3":
-      content = <h3 className={`${className?.join(" ") || ''} font-bold  xl:text-2xl lg:text-xl md:text-lg`}>{children}</h3>;
+    case 'H3':
+      content = (
+        <h3
+          className={`${className?.join(' ') || ''} font-bold  xl:text-2xl lg:text-xl md:text-lg`}
+        >
+          {title}
+        </h3>
+      );
       break;
-    case "H4":
-      content = <h4 className={`${className?.join(" ") || ''} font-bold  xl:text-xl lg:text-lg`}>{children}</h4>;
+    case 'H4':
+      content = (
+        <h4
+          className={`${className?.join(' ') || ''} font-bold  xl:text-xl lg:text-lg text-base`}
+        >
+          {title}
+        </h4>
+      );
       break;
-    case "H5":
-      content = <h5 className={`${className?.join(" ") || ''} font-bold  xl:text-lg lg:text-base`}>{children}</h5>;
+    case 'H5':
+      content = (
+        <h5
+          className={`${className?.join(' ') || ''} font-bold  xl:text-lg lg:text-base`}
+        >
+          {title}
+        </h5>
+      );
       break;
-    case "H6":
-      content = <h6 className={`${className?.join(" ") || ''} font-bold  xl:text-base`}>{children}</h6>;
+    case 'H6':
+      content = (
+        <h6 className={`${className?.join(' ') || ''} font-bold  xl:text-base`}>
+          {title}
+        </h6>
+      );
       break;
-    case "p":
-        content = <p className={`${className?.join(" ") || ''}`}>{children}</p>
+    case 'p':
+      content = <p className={`${className?.join(' ') || ''}`}>{title}</p>;
     default:
-      content
+      content;
   }
 
   return <>{content}</>;
 }
-
