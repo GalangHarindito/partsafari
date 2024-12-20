261ca10b8cd550660cc2106c442aeba2e8f91340
GalangHarindito <galang.harindito@gmail.com>
2024-12-20 15:39:31 +0700

    [component]fixing unit test

--- src/app/shared/components/checkbox/__test__/checkbox.test.tsx
+++ src/app/shared/components/checkbox/__test__/checkbox.test.tsx
@@ -1,31 +1,30 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import Checkbox from "../checkbox";
-import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from '@testing-library/react';
+import Checkbox from '../checkbox';
+import '@testing-library/jest-dom';
 
-describe("Checkbox Component", () => {
-  test("Checkbox render with initial state true", () => {
-    render(<Checkbox check={true} label={""} />);
-    const checkbox = screen.getByRole("checkbox");
+describe('Checkbox Component', () => {
+  test('Checkbox render with initial state true', () => {
+    render(<Checkbox check={true} label={''} />);
+    const checkbox = screen.getByRole('checkbox');
     expect(checkbox).toBeChecked();
     expect(checkbox).toMatchSnapshot();
   });
 
-  test("Checkbox render with label", () => {
-    render(<Checkbox check={true} label={"test"} />);
-    const label = screen.getByLabelText("test")
+  test('Checkbox render with label', () => {
+    render(<Checkbox check={true} label={'test'} />);
+    const label = screen.getByLabelText(/test/i);
     expect(label).toBeInTheDocument();
   });
 
-
-  test("Checkbox render with initial state false", () => {
-    render(<Checkbox check={false} label={""} />);
-    const checkbox = screen.getByRole("checkbox");
+  test('Checkbox render with initial state false', () => {
+    render(<Checkbox check={false} label={''} />);
+    const checkbox = screen.getByRole('checkbox');
     expect(checkbox).not.toBeChecked();
   });
 
-  test("Toggles Checkbox", () => {
-    render(<Checkbox check={false} label={""} />);
-    const checkbox = screen.getByRole("checkbox");
+  test('Toggles Checkbox', () => {
+    render(<Checkbox check={false} label={''} />);
+    const checkbox = screen.getByRole('checkbox');
 
     expect(checkbox).not.toBeChecked();
 
